00e91a41fc73fca2ce9ec6f89466a1064eb4b8f0
Debbl <me@aiwan.run>
2023-09-08 11:08:10 +0800

    feat: useLatest add comment

--- packages/hooks/src/useLatest/index.ts
+++ packages/hooks/src/useLatest/index.ts
@@ -1,5 +1,10 @@
 import { useRef } from "react";
 
+/**
+ *  Returns a ref object whose `.current` property is always set to the latest value passed to `useLatest`.
+ * @param value - The value to be stored in the ref
+ * @returns A ref object whose `.current` property is always set to the latest value passed to `useLatest`
+ */
 const useLatest = <T>(value: T) => {
   const ref = useRef(value);
   ref.current = value;
