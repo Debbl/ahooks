5f945794e2255ce2462dd91b7a49dfdea0a7aef2
Debbl <me@aiwan.run>
2023-09-09 12:41:15 +0800

    test: use react-hooks test SSR

--- packages/hooks/src/useHydrated/index.test.ts
+++ packages/hooks/src/useHydrated/index.test.ts
@@ -1,16 +1,40 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook as renderHookSSR } from "@testing-library/react-hooks/server";
+import { renderHook as renderHookCSR } from "@testing-library/react-hooks/dom";
 import useHydrated from ".";
 
-const setUp = () => renderHook(() => useHydrated());
+const setUpSSR = () => renderHookSSR(() => useHydrated());
+const setUpCSR = () => renderHookCSR(() => useHydrated());
 
 describe("useLatest", () => {
-  it("should return isBrowser true", () => {
-    const { result } = setUp();
+  it("should return isServer true when rendering on the server", () => {
+    const { result } = setUpSSR();
+    expect(result.current.isServer).toBe(true);
+  });
+
+  it("should return isBrowser false when rendering on the server", () => {
+    const { result } = setUpSSR();
+    expect(result.current.isBrowser).toBe(false);
+  });
+
+  it("should return isServer false when hydrated", () => {
+    const { result, hydrate } = setUpSSR();
+    hydrate();
+    expect(result.current.isServer).toBe(false);
+  });
+
+  it("should return isBrowser true when hydrated", () => {
+    const { result, hydrate } = setUpSSR();
+    hydrate();
     expect(result.current.isBrowser).toBe(true);
   });
 
-  it("should return isServer false", () => {
-    const { result } = setUp();
+  it("should return isServer false on the client", () => {
+    const { result } = setUpCSR();
     expect(result.current.isServer).toBe(false);
+  });
+
+  it("should return isBrowser true on the client", () => {
+    const { result } = setUpCSR();
+    expect(result.current.isBrowser).toBe(true);
   });
 });
