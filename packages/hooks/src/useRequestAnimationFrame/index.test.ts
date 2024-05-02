6412ff451491a46e1367211dea8c57a858e2e6ad
Brendan Dash <me@aiwan.run>
2024-05-02 22:59:13 +0800

    feat: add useAnimationFrame

---
+++ packages/hooks/src/useRequestAnimationFrame/index.test.ts
@@ -0,0 +1,11 @@
+import { renderHook } from "@testing-library/react";
+import { useRequestAnimationFrame } from ".";
+
+describe("useRequestAnimationFrame", () => {
+  it("should return a ref object", () => {
+    const { result } = renderHook(() =>
+      useRequestAnimationFrame(() => () => {}),
+    );
+    expect(result.current).toBe(undefined);
+  });
+});
